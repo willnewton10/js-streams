c1958e38986c9235f96d6858bd92c8105415617d
Will Newton <willnewton10@gmail.com>
2015-10-04 12:59:52 -0400

    added timed queue

--- main.js
+++ main.js
@@ -20,73 +20,84 @@ function clicked() {
 	A.event(Math.random()); 
 }
 
-var B = A.create(SIDE_EFFECT, clear)
-	.create(TRANSFORM, function (event) { 
+var B = A.sideEffect(clear)
+	.transform(function (event) { 
 		return (event + "").slice(0,5);
 	});
 	
-var D = B.create(AGGREGATOR, function (current, event) {
+var D = B.aggregator(function (current, event) {
 		return current + "::" + event;
 	}, "INITIAL VALUE")
-	.create(SIDE_EFFECT, colorPrinter("yellow"))
+	.sideEffect(colorPrinter("yellow"))
 
 var filter = function (event) {
 	return event.indexOf("4") > 2; 
 };
 
-var C = B.create(FILTER, filter)
-	.create(SIDE_EFFECT, print);
+var C = B.filter(filter)
+	.sideEffect(print);
 
-var E = B.create(FILTER, inverse(filter))
-	.create(SIDE_EFFECT, colorPrinter("pink"));
+var E = B.filter(inverse(filter))
+	.sideEffect(colorPrinter("pink"));
 	
-var F = B.create(AGGREGATOR, function (a, b) { return a + 1; }, 0)
-	.create(SIDE_EFFECT, colorPrinter("green", "count: "));
+var F = B.aggregator(function (a, b) { return a + 1; }, 0)
+	.sideEffect(colorPrinter("green", "count: "));
 
-var G = A.create(BUFFER, function (buf, e) {
+var G = A.buffer(function (buf, e) {
 		return buf.length == 3;
 	}, { insertBefore: true })
-	.create(SIDE_EFFECT, colorPrinter("red", "group of 3: "));
+	.sideEffect(colorPrinter("red", "group of 3: "));
 
-var F = A.create(BUFFER, function (buf, e) {
+var F = A.buffer(function (buf, e) {
 		if (buf.length == 0) return false;
 		var last = buf[buf.length - 1];
 		return last > e; // send if new event is lower than prev. Should produce an increasing collection. 
-	});
-F.create(SIDE_EFFECT, colorPrinter("blue", "all increasing:"));
+	})
+	.sideEffect(colorPrinter("blue", "all increasing:"));
 
-A.create(BUFFER, function (buf, evt, stream) {
+A.buffer(function (buf, evt, stream) {
 	var prev = stream.lastEventTime;
 	var now = Date.now();
 	stream.lastEventTime = now;
 	if (!prev) return false;
 	return now - prev > 5000; // returns buffer if last event was longer than 5 seconds ago
 	
-}).create(SIDE_EFFECT, colorPrinter("orange", "it has been 5 seconds: "));
+}).sideEffect(colorPrinter("orange", "it has been 5 seconds: "));
 
-A.create(BUFFER, function (buf, evt, stream) {
+A.buffer(function (buf, evt, stream) {
 	var prev = stream.lastEventTime;
 	var now = Date.now();
 	stream.lastEventTime = now;
 	return now - prev < 250;
 }, { insertBefore: true })
-.create(TRANSFORM, function (b) {
+.transform(function (b) {
 	return [b[b.length - 2], b[b.length - 1]];
 })
-.create(SIDE_EFFECT, colorPrinter("black;background-color:green", "less than 250ms"));
+.sideEffect(colorPrinter("black;background-color:green", "less than 250ms"));
 
-var Z = A.create(CONTROLLER, function (event) {
+var Z = A.controller(function (event) {
 	return event > 0.5 ? "X": "Y";
 }, {
-	"X": new Stream(SIDE_EFFECT, colorPrinter("blue;background-color:white", "x > .5  ")),
-	"Y": new Stream(SIDE_EFFECT, colorPrinter("red;background-color:white", "x <= .5 "))
+	"X": new Stream(types.SIDE_EFFECT, colorPrinter("blue;background-color:white", "x > .5  ")),
+	"Y": new Stream(types.SIDE_EFFECT, colorPrinter("red;background-color:white", "x <= .5 "))
 });
 
 var Q = A
-	.create(TRANSFORM, function (d) { return d * 10; })
-	.create(DELAY, function (d) { return d; })
-	.create(SIDE_EFFECT, colorPrinter("black;background-color:yellow;", "waited seconds: "));
+	.transform(function (d) { return d * 10; })
+	.delay(function (d) { return d; })
+	.sideEffect(colorPrinter("black;background-color:yellow;", "waited seconds: "));
 
+var R = createStreamFromArray(["hello", "how", "are", "you", "doing", "today?"]);
+R.interval(2)
+	.sideEffect(colorPrinter("white;background-color:red;", "from array: "));
+R.start();
+
+var V = new Stream(types.SIDE_EFFECT, colorPrinter("white;background-color:blue;", "from array: "))
+var U = A
+	.transform(function (a) { return a*10})
+	.interval(1)
+	.addSubscriber(V);
+	
 function inverse(f) {
 	return function (arg) {
 		return !f(arg);
--- stream.js
+++ stream.js
@@ -5,42 +5,63 @@ function Stream(type, func, other) {
 	this.subscribers = []; 
 };
 
-Stream.prototype.subscribe = function (subscriber) { 
+Stream.prototype.addSubscriber = function (subscriber) { 
 	this.subscribers.push(subscriber); 
 };
 Stream.prototype.subscribeTo = function (source) { 
-	source.subscribe(this); 
+	source.addSubscriber(this); 
 	return this; 
 };
 
-var FILTER = "filter",
-	TRANSFORM = "transform",
-	SIDE_EFFECT = "sideEffect",
-	AGGREGATOR = "aggregator",
-	BUFFER = "buffer",
-	CONTROLLER = "controller",
-	DELAY = "delay";
+// types of streams
+var types = {
+	FILTER: "filter",
+	TRANSFORM: "transform",
+	SIDE_EFFECT: "sideEffect",
+	AGGREGATOR: "aggregator",
+	BUFFER: "buffer",
+	CONTROLLER: "controller",
+	DELAY: "delay",
+	INTERVAL: "interval"
+};
 
+Object.keys(types).forEach(function (key) {
+	var type = types[key];
+	Stream.prototype[type] = function (func, other) {
+		return this.create(type, func, other);
+	}
+});
+
+Stream.prototype.event = function (e) { //receiving an event that it might send off	
+	if (this.type) {
+		this["_"+this.type](e, this.other);
+	} else {
+		this.send(e);
+	}
+};
+Stream.prototype.create = function (type, func, other) {
+	return new Stream(type, func, other).subscribeTo(this);
+};
 Stream.prototype.send = function (event) {
 	this.subscribers.forEach(function (subscriber) {
 		subscriber.event(event);
 	});
 };
-Stream.prototype[SIDE_EFFECT] = function (e) {
+Stream.prototype["_"+types.SIDE_EFFECT] = function (e) {
 	this.f(e);
 	this.send(e);
 }
-Stream.prototype[FILTER] = function (e) {
+Stream.prototype["_"+types.FILTER] = function (e) {
 	if (this.f(e)) this.send(e);
 }
-Stream.prototype[TRANSFORM] = function (e) {
+Stream.prototype["_"+types.TRANSFORM] = function (e) {
 	this.send(this.f(e));
 }
-Stream.prototype[AGGREGATOR] = function (e) {
+Stream.prototype["_"+types.AGGREGATOR] = function (e) {
 	this.other = this.f(this.other, e);
 	this.send(this.other);
 }
-Stream.prototype[BUFFER] = function (e) {
+Stream.prototype["_"+types.BUFFER] = function (e) {
 	// buffer function should return true if it should send the buffer now without the new event,
 	// false if it should add the event to the buffer and not send the event yet.
 	if (this._buf == undefined) { this._buf = []; }
@@ -63,24 +84,74 @@ Stream.prototype[BUFFER] = function (e) {
 		this._buf.push(e);
 	}
 }
-Stream.prototype[CONTROLLER] = function (e) {
+Stream.prototype["_"+types.CONTROLLER] = function (e) {
 	var which = this.f(e);
 	this.other[which].event(e);
 };
-Stream.prototype[DELAY] = function (e) {
+Stream.prototype["_"+types.DELAY] = function (e) {
 	var secondsToWait = (typeof this.f == "number") ? this.f : this.f(e);
 	var self = this;
 	setTimeout(function () {
 		self.send(e);
 	}, secondsToWait * 1000); // milliseconds
 }
-Stream.prototype.event = function (e) { //receiving an event that it might send off	
-	if (this.type) {
-		this[this.type](e, this.other);
+Stream.prototype["_"+types.INTERVAL] = function (e) {
+	/*
+	Timed Queue:
+	
+		This stream collects input into a Q and releases it
+		after a given interval of time (or greater) has elapsed
+		since the last element was released to subscribers.
+	
+	We receive an event:
+								Waiting
+		Q						Yes				No
+		no elements				+q (t. exists)	send e, start t.waiting=true
+		already has elements    +q (t. exists)	-
+	
+	A timer finishes?
+	
+		Q has elements?			-q, send e, start t.
+		Q no elements?			waiting = false;
+	*/
+	var secondsToWait = (typeof this.f == "number") ? this.f : this.f(e);
+	this.q = this.q || [];
+	var q = this.q;
+	this.waiting = this.waiting || false;
+	
+	var self = this;
+	function startTimer() {
+		setTimeout(function () {
+			console.log("waited " + (secondsToWait) + " seconds");
+			if (q.length > 0) {
+				var e = q.splice(0,1);
+				console.log("q", q);
+				self.send(e);
+				startTimer();
+			} else {
+				self.waiting = false;
+			}
+		}, secondsToWait * 1000);
+	}
+	
+	if (this.waiting) {
+		q.push(e);
 	} else {
 		this.send(e);
+		this.waiting = true;
+		startTimer();
 	}
-};
-Stream.prototype.create = function (type, func, other) {
-	return new Stream(type, func, other).subscribeTo(this);
-};
+}
+Stream.prototype.start = function () {
+	var array = this.other;
+	var self = this;
+	array.forEach(function (element) {
+		self.send(element);
+	});
+}
+
+function createStreamFromArray(array) {
+	var stream = new Stream();
+	stream.other = array;
+	return stream;
+}
